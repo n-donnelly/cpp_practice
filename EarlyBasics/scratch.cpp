6938c8b54386d6b17588f517c2310543f36f8eb2
Neil <n.donnellyv3.0@gmail.com>
2023-08-22 22:04:41 +0100

    Adding directives to scratch

--- EarlyBasics/scratch.cpp
+++ EarlyBasics/scratch.cpp
@@ -1,7 +1,26 @@
+//'#' is a preprocessor directive - these tell preprocesser to do something
+//Preprocessor directives are executed before the code is compiled
+//Include directive tells preprocessor to include the contents of the file iostream
 #include <iostream>
+
+#define MY_NAME "Neil" //Preprocessor directive to define a macro. This macro is replaced with the string "Neil" before the code is compiled
+
+//Considered a bad practice as can cause name collisions
+using namespace std; //Explicitly using the std namespace
+
+#define sum(a, b) (a + b) //Macro to define a function. This macro is replaced with the expression (a + b) before the code is compiled
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+    cout << "Hello, world!" << endl; //Since we are using the std namespace, we don't need to use std::cout
+    cout << "My name is " << MY_NAME << endl; //Since MY_NAME is a macro, it is replaced with the string "Neil" before the code is compiled
+
+#define check
+#ifdef check
+    cout << "check is defined" << endl;
+#endif
+
+    cout << sum(1, 2) << endl; //Since sum is a macro, it is replaced with the expression (1 + 2) before the code is compiled
+
     return 0;
 }
