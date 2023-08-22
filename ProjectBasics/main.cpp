448761ab6359916f2605ed467c50ddabe3b5b41f
Neil <n.donnellyv3.0@gmail.com>
2023-08-22 21:25:59 +0100

    Early project basics

---
+++ ProjectBasics/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int add(int x, int y); // function prototype (declaration)
+
+int main()
+{
+    std::cout << "The sum of 3 and 4 is: " << add(3, 4) << std::endl;
+    return 0;
+}
